e245bcb1e60347056ccb92884b38aff2e1ba308d
xdanto <XDanto@cranbrook.edu>
2026-06-08 14:51:35 -0400

    Update viewer_helper.rb

--- public/helpers/viewer_helper.rb
+++ public/helpers/viewer_helper.rb
@@ -8,9 +8,6 @@ class ViewerHelper
       file_uri.gsub!(/http(s)?:\/\//,'')
       array = file_uri.split('/')
       return "https://#{array[0]}/digital/api/singleitem/collection/#{array[3].gsub!(':','/id/')}/thumbnail"
-    elsif file_uri.include?("kaltura.com")
-      entry_id = file_uri.split("/")[-1]
-      return "https://cfvod.kaltura.com/p/2356971/sp/235697100/thumbnail/entry_id/#{entry_id}"
     else
       return file_uri
     end
